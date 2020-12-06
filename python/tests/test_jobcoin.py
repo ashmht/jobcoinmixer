a9ccf5b57a25c7a493c4efd898e5d0cbbb3b041e
Ashish Mehta <ashish.mehta@carta.com>
2020-12-06 12:04:51 -0800

    Ran black

--- python/tests/test_jobcoin.py
+++ python/tests/test_jobcoin.py
@@ -10,25 +10,26 @@ from .. import cli
 @pytest.fixture
 def response():
     import requests
-    return requests.get('https://jobcoin.gemini.com/')
+
+    return requests.get("https://jobcoin.gemini.com/")
 
 
 def test_content(response):
-    assert 'Hello!' in str(response.content)
+    assert "Hello!" in str(response.content)
 
 
 def test_cli_basic():
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert 'Welcome to the Jobcoin mixer' in result.output
+    assert "Welcome to the Jobcoin mixer" in result.output
 
 
 def test_cli_creates_address():
     runner = CliRunner()
-    address_create_output = runner.invoke(cli.main, input='1234,4321').output
+    address_create_output = runner.invoke(cli.main, input="1234,4321").output
     output_re = re.compile(
-        r'You may now send Jobcoins to address [0-9a-zA-Z]{32}. '
-        'They will be mixed and sent to your destination addresses.'
+        r"You may now send Jobcoins to address [0-9a-zA-Z]{32}. "
+        "They will be mixed and sent to your destination addresses."
     )
     assert output_re.search(address_create_output) is not None
