3fc32a2c20523dad8d9e7a8d166a1991f1d87435
Ashish Mehta <ashish.mehta@carta.com>
2021-01-10 14:50:16 -0800

    Fixes to jobcoin mixer

--- python/jobcoin/mixer.py
+++ python/jobcoin/mixer.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from typing import Tuple
 
 from jobcoin.addresses import Addresses
 from jobcoin.doler import Doler
@@ -11,28 +12,26 @@ class Mixer:
         self.jobcoin = jobcoin
 
     @staticmethod
-    def detect_transfer(deposit_address) -> Decimal:
+    def detect_transfer(deposit_address: str) -> Tuple[bool, str]:
         # Get balance and transactions from deposit address
         balance, transactions = Addresses.get_balance_transactions(
             deposit_address=deposit_address
         )
         print(f"Deposit address has balance: {balance}")
-        return Decimal(balance)
+        return bool(Decimal(balance)), balance
 
-    def mix(
-        self, big_house_address: str, customer_id: int,
-    ):
+    def mix(self, big_house_address: str, customer_id: int):
         deposit_address = self.jobcoin.get_deposit_address(customer_id=customer_id)
         customer_addresses = self.jobcoin.get_customer_addresses(
             customer_id=customer_id
         )
-        amount: Decimal = self.detect_transfer(deposit_address=deposit_address)
-        if amount > 0:
+        has_balance, amount = self.detect_transfer(deposit_address=deposit_address)
+        if has_balance:
             Transactions.transfer_jobcoins(
                 source=deposit_address, destination=big_house_address, amount=amount,
             )
             Doler.dole(
-                big_house_address,
+                big_house_address=big_house_address,
                 amount=Decimal(amount),
                 customer_addresses=customer_addresses,
             )
